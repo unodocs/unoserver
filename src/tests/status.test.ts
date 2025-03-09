b263385f46dbb64bea5c8431ae9d92347fbf0266
Maxim Kerstens <maxim.kerstens@gmail.com>
2025-03-09 19:18:47 +0100

    style: fix

--- src/tests/status.test.ts
+++ src/tests/status.test.ts
@@ -4,14 +4,14 @@ import type { File as UploadedFile } from '../plugins/store.js'
 
 import { startTestServer } from './startTestServer.js'
 
-const rtfFile: UploadedFile =  {
+const rtfFile: UploadedFile = {
 	fieldname: 'file',
 	originalname: 'someUploadedFile.docx',
 	encoding: 'utf8',
 	mimetype: 'word/doc',
 	size: 213400,
 	filename: 'someUploadedFile.docx',
-	path: 'tmp/someUploadedFile.docx'
+	path: 'tmp/someUploadedFile.docx',
 }
 
 const testServer = await startTestServer()
@@ -19,20 +19,17 @@ const testServer = await startTestServer()
 afterAll(async () => testServer.close())
 
 test('/status', async () => {
-	const uuid = "xxxCreate";
-	const toFormat = 'pdf';
-	testServer.store.register(uuid, rtfFile, toFormat);
+	const uuid = 'xxxCreate'
+	const toFormat = 'pdf'
+	testServer.store.register(uuid, rtfFile, toFormat)
 
 	const response = await testServer.fetch(`status`, {
 		method: 'GET',
 	})
 
-	expect(response.headers.get('content-type')).toBe(
-		'application/json; charset=utf-8',
-	)
+	expect(response.headers.get('content-type')).toBe('application/json; charset=utf-8')
 
-
-	const payload: any = await response.json();
+	const payload: any = await response.json()
 	testServer.log.debug(payload)
 
 	expect(payload.documents.length).toBe(1)
@@ -41,5 +38,4 @@ test('/status', async () => {
 	expect(payload.documents[0].name).toBe(rtfFile.originalname)
 	expect(payload.documents[0].format).toBe(toFormat)
 	expect(payload.documents[0].size).toBe(rtfFile.size)
-
 }, 30000)
